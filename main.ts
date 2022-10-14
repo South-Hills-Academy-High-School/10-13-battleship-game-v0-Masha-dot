fff6df6a5f45702ca3ec635aa6e371c303ac03a6
Masha-dot <113388272+Masha-dot@users.noreply.github.com>
2022-10-13 22:09:06 -0700

    move

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
-let boatArray: Sprite[] = []
-function moveBoat (BoatArray: any[]) {
-    if (grid.spriteRow(cursor) == 6) {
+function moveBoat (boatArray: Sprite[]) {
+    if (grid.spriteCol(cursor) == 6) {
         grid.move(cursor, 0, -1)
     }
     Iterator = 0
     cursor.setFlag(SpriteFlag.Invisible, true)
     for (let value of boatArray) {
-        grid.place(value, tiles.getTileLocation(grid.spriteCol(cursor) + 0, grid.spriteRow(cursor) + Iterator))
-        Iterator = 1
+        grid.place(value, tiles.getTileLocation(grid.spriteCol(cursor), grid.spriteRow(cursor) + Iterator))
+        Iterator += 1
     }
     if (controller.A.isPressed()) {
         cursor.setFlag(SpriteFlag.Invisible, false)
@@ -17,9 +16,9 @@ function moveBoat (BoatArray: any[]) {
 let Iterator = 0
 let cursor: Sprite = null
 let moveBoatflag = 0
-let text_list = ["up", "up", "up"]
+let boatRotateArray = ["up", "up", "up"]
 let currentBoat = 0
-let smallBoatArray = [[sprites.create(img`
+let boatSpriteArray = [[sprites.create(img`
     . . . . . b b b b b b . . . . . 
     . . . b b 9 9 9 9 9 9 b b . . . 
     . . b b 9 9 9 9 9 9 9 9 b b . . 
@@ -87,47 +86,46 @@ let smallBoatArray = [[sprites.create(img`
     . . b d 5 d 3 3 3 3 5 5 b b . . 
     . . . b b 5 5 5 5 5 5 b b . . . 
     . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Player), sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
     `, SpriteKind.Player)]]
-scene.setBackgroundImage(img`
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    `)
+tiles.setCurrentTilemap(tilemap`level2`)
 moveBoatflag = 1
 cursor = sprites.create(img`
-    3 3 3 3 3 . . . . . . 3 3 3 3 3 
-    3 . . . . . . . . . . . . . . 3 
-    3 . . . . . . . . . . . . . . 3 
-    3 . . . . . . . . . . . . . . 3 
-    3 . . . . . . . . . . . . . . 3 
-    3 . . . . . . . . . . . . . . 3 
-    3 . . . . . . . . . . . . . . 3 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    3 . . . . . . . . . . . . . . . 
-    3 . . . . . . . . . . . . . . 3 
-    3 . . . . . . . . . . . . . . 3 
-    3 . . . . . . . . . . . . . . 3 
-    3 . . . . . . . . . . . . . . 3 
     3 3 3 3 3 . . . . . 3 3 3 3 3 3 
+    3 . . . . . . . . . . . . . . 3 
+    3 . . . . . . . . . . . . . . 3 
+    3 . . . . . . . . . . . . . . 3 
+    3 . . . . . . . . . . . . . . 3 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    3 . . . . . . . . . . . . . . 3 
+    3 . . . . . . . . . . . . . . 3 
+    3 . . . . . . . . . . . . . . 3 
+    3 . . . . . . . . . . . . . . 3 
+    3 . . . . . . . . . . . . . . 3 
+    3 3 3 3 3 . . . . . . 3 3 3 3 3 
     `, SpriteKind.Player)
 grid.snap(cursor)
 grid.moveWithButtons(cursor)
 game.onUpdate(function () {
     if (moveBoatflag == 1) {
-        let boatSpriteArray: Array[] = []
         moveBoat(boatSpriteArray[currentBoat])
     }
 })
